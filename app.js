4b89ec8e25678722de2d8566fc178486642291d1
Yuugen64 <yuugen64@protonmail.com>
2020-11-03 19:42:14 -0600

    [007-0.0.4]: Added click/tap toggle >> For the mobile version of the site it makes sense for the user to tap on the individual sections and then scroll through the information there which is too long to display fully on the site normally.

--- app.js
+++ app.js
@@ -1,1 +1,42 @@
-console.log("[app.js]: Attached and working correctly!");
+console.log("[app.js]: Attached and working properly!");
+
+// ===== Object interaction references. =====
+const projects = document.getElementById("projects");
+const challenges = document.getElementById("challenges");
+const contact = document.getElementById("contact");
+
+
+
+
+// ===== Functions =====
+
+let attachEventListeners = () => {
+    console.log("Click listeners attached.");
+
+    //Projects section
+    projects.addEventListener("click", () => {
+        console.log("Click registered on projects!");
+
+        projects.classList.toggle("grow");
+    });
+
+    //Challenges section
+    challenges.addEventListener("click", () => {
+        console.log("Click registered on projects!");
+
+        challenges.classList.toggle("grow");
+    });
+
+    //Contact section
+    contact.addEventListener("click", () => {
+        console.log("Click registered on projects!");
+
+        contact.classList.toggle("grow");
+    });
+};
+
+
+
+
+
+document.onload = attachEventListeners();
